050df93679175ee2980ff665f67586e4024f2e03
Nuthan Santharam <nuthankumarns@gmail.com>
2013-04-23 19:56:46 +0530

    AccordionList loaded from Ajax
    Updated store/Task.js shows loaded data from Ajax request

--- app/store/Task.js
+++ app/store/Task.js
@@ -1,67 +1,63 @@
 Ext.define('AccordionListExample.store.Task', {
-    extend: 'Ext.data.TreeStore',
-    requires: [
-        'AccordionListExample.model.Task'
-    ],
-    config: {
-        defaultRootProperty: 'items',
-        model: 'AccordionListExample.model.Task',
-        autoLoad: true,
+extend: 'Ext.data.TreeStore',
+requires: [
+'AccordionListExample.model.Task'
+],
 
-        // XXX: AccordionList doesn't show when loaded from JSON
-        // proxy: {
-        //     type: 'ajax',
-        //     url: 'resources/data/testData.json',
-        //     reader: {
-        //         type: 'json',
-        //         rootProperty: 'items'
-        //      }
-        // }
 
-              root: {
-                items: [{
-                  text: 'Today',
-                  items: [{
-                    text: 'Eat',
-                    leaf: true
-                  }, {
-                    text: 'Sleep',
-                    leaf: true
-                  }, {
-                    text: 'Drinking',
-                    leaf: true
-                  }]
-                }, {
-                  text: 'Tomorrow',
-                  items: [{
-                    text: 'Watch TV',
-                    leaf: true
-                  }, {
-                    text: 'Watch Video',
-                    leaf: true
-                  }]
-                }, {
-                  text: 'This week',
-                  items: [{
-                    text: 'Shopping',
-                    leaf: true
-                  }]
-                }, {
-                  text: 'Later',
-                  items: [{
-                    text: 'Study',
-                    items: [{
-                      text: 'Eat',
-                      leaf: true
-                    }, {
-                      text: 'Sleep',
-                      leaf: true
-                    }, {
-                      text: 'Drinking',
-                      leaf: true
-                    }]
-                  }]
-                }]
-              }
-            }
+onProxyLoad: function(operation) {
+var me = this,
+records = operation.getRecords(),
+successful = operation.wasSuccessful(),
+node = operation.getNode();
+
+
+node.beginEdit();
+node.set('loading', false);
+if (successful) {
+records = me.fillNode(node, records);
+}
+node.endEdit();
+this.updateNode(node);
+me.loading = false;
+me.loaded = true;
+
+
+node.fireEvent('load', node, records, successful);
+me.fireEvent('load', this, records, successful, operation);
+
+
+// this is a callback that would have been passed to the 'read' function and is
+// optional
+Ext.callback(operation.getCallback(), operation.getScope() || me, [records, operation, successful]);
+},
+
+
+
+onNodeBeforeExpand: function(node, options, e) {
+
+},
+
+config: {
+defaultRootProperty: 'items',
+model: 'AccordionListExample.model.Task',
+autoLoad: true,
+
+
+// XXX: AccordionList Now show data from JSON
+proxy: {
+type: 'ajax',
+url: 'resources/data/testData.json'// ,
+// reader: {
+// type: 'json',
+// rootProperty: 'items'
+// }
+},
+listeners: {
+load: function(store, records, successful, operation, eOpts) {
+console.log(records.length);
+console.log(store.getCount());
+}
+} 
+}
 });
