e531b6e934460ec7882b7ba5f471ca14fb648fc5
kawanoshinobu <kawanoshinobu@gmail.com>
2013-04-24 15:55:03 +0900

    Change indentation etc.

--- app/store/Task.js
+++ app/store/Task.js
@@ -1,63 +1,54 @@
 Ext.define('AccordionListExample.store.Task', {
-extend: 'Ext.data.TreeStore',
-requires: [
-'AccordionListExample.model.Task'
-],
+    extend: 'Ext.data.TreeStore',
+    requires: [
+        'AccordionListExample.model.Task'
+    ],
 
+    config: {
+        defaultRootProperty: 'items',
+        model: 'AccordionListExample.model.Task',
+        autoLoad: true,
 
-onProxyLoad: function(operation) {
-var me = this,
-records = operation.getRecords(),
-successful = operation.wasSuccessful(),
-node = operation.getNode();
+        // XXX: AccordionList Now show data from JSON
+        proxy: {
+            type: 'ajax',
+            url: 'resources/data/testData.json'
+        },
 
+        listeners: {
+            load: function(store, records, successful, operation, eOpts) {
+                console.log(records.length);
+                console.log(store.getCount());
+            }
+        }
+    },
 
-node.beginEdit();
-node.set('loading', false);
-if (successful) {
-records = me.fillNode(node, records);
-}
-node.endEdit();
-this.updateNode(node);
-me.loading = false;
-me.loaded = true;
+    onProxyLoad: function(operation) {
+        var me = this,
+            records = operation.getRecords(),
+            successful = operation.wasSuccessful(),
+            node = operation.getNode();
 
+        node.beginEdit();
+        node.set('loading', false);
+        if (successful) {
+            records = me.fillNode(node, records);
+        }
+        node.endEdit();
+        this.updateNode(node);
+        me.loading = false;
+        me.loaded = true;
 
-node.fireEvent('load', node, records, successful);
-me.fireEvent('load', this, records, successful, operation);
+        node.fireEvent('load', node, records, successful);
+        me.fireEvent('load', this, records, successful, operation);
 
+        // this is a callback that would have been passed to the 'read' function and is
+        // optional
+        Ext.callback(operation.getCallback(), operation.getScope() || me, [records, operation, successful]);
+    },
 
-// this is a callback that would have been passed to the 'read' function and is
-// optional
-Ext.callback(operation.getCallback(), operation.getScope() || me, [records, operation, successful]);
-},
+    onNodeBeforeExpand: function(node, options, e) {
 
+    }
 
-
-onNodeBeforeExpand: function(node, options, e) {
-
-},
-
-config: {
-defaultRootProperty: 'items',
-model: 'AccordionListExample.model.Task',
-autoLoad: true,
-
-
-// XXX: AccordionList Now show data from JSON
-proxy: {
-type: 'ajax',
-url: 'resources/data/testData.json'// ,
-// reader: {
-// type: 'json',
-// rootProperty: 'items'
-// }
-},
-listeners: {
-load: function(store, records, successful, operation, eOpts) {
-console.log(records.length);
-console.log(store.getCount());
-}
-} 
-}
 });
