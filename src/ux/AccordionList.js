6700b27e35f3f3f2100f7f9f5748becd005e49c5
Shinobu KAWANO <kawanoshinobu@gmail.com>
2013-09-18 07:48:40 +0900

    fixed the problem that occurred by #21.
    It reported by #19

--- src/ux/AccordionList.js
+++ src/ux/AccordionList.js
@@ -618,11 +618,11 @@ Ext.define('Ext.ux.AccordionList', {
             me.readyAnimation(list, index, target, record, e);
         }
 
-        if (!me.fireEvent('itemtap', me, list, index, target, record, e))
-    	{
-		// if any of the event handlers return 'false', we cancel processing of the tap
-        	return;
-    	}
+        // if any of the event handlers return 'false', we cancel processing of the tap
+        var ret = me.fireEvent('itemtap', me, list, index, target, record, e);
+        if (ret === false) {
+            return;
+        }
 
         if (node.isLeaf()) {
             me.fireEvent('leafitemtap',
